5f097c9b6e2416802cea01006b79d9e4fc045f37
azureuser <azureuser@partsunlimitedmrp.local>
2018-09-13 14:10:40 +0100

    added templates

---
+++ InstallAppswithChocolateyWin10.ps1
@@ -0,0 +1,40 @@
+
+#Install Chocolatey
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+
+
+#Assign Packages to Install
+
+$Packages = 'googlechrome',`
+
+            'vsocode',`
+
+            'git',`
+
+            'visualstudio2017-workload-azure',`
+
+            'visualstudio2017-workload-netweb'
+
+            'docker-for-windows'
+
+            'Microsoft-Hyper-V -s windowsfeatures'  
+
+
+
+
+
+#Install Packages
+
+ForEach ($PackageName in $Packages)
+
+{choco install $PackageName -y}
+
+
+
+#Reboot
+
+Restart-Computer
